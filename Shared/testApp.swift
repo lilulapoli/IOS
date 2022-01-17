30e06210397a3b92a9ec9036a13f17e11449bde7
lilulapoli <97735600+lilulapoli@users.noreply.github.com>
2022-01-17 15:38:22 +0300

    init

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Лилия on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
